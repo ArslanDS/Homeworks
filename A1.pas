5423d4891437d9aad59a83c69d9a072235813368
Arslan <Arslan98671@mail.ru>
2015-12-14 12:20:38 +0300

    Update A1.pas

--- A1.pas
+++ A1.pas
@@ -24,12 +24,12 @@ begin
   end;
   for i := k downto k - y.Length + 1 do
   begin
-    c[i] := char(ord(c[i]) + ord(y[i - lol + y.Length]) - 48);
+    c[i] := char(ord(c[i]) + ord(y[i - k + y.Length]) - 48);
     c[i - 1] := char(ord(c[i - 1]) + ((ord(c[i]) - 48) div 10));
     c[i] := char(((ord(c[i]) - 48) mod 10) + 48);
   end;
   if test > 9 then k := 0 else k := 1;
-  for i := k to lol do
+  for i := k to k do
     write(c[i]);  
 end;
 
@@ -39,9 +39,9 @@ begin
   test := ord(a[1]) + ord(b[1]) - 96;
   if a.Length > b.length then
   begin
-    lol := a.Length;
-    setlength(c, lol + 1);
-    for i := 0 to lol do
+    k := a.Length;
+    setlength(c, k + 1);
+    for i := 0 to k do
       c[i] := '0';
     sum(a, b, c);
   end
