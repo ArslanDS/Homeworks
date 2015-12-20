8d38b5d23d762d6f15bdf4cfb6bac094f46b11bc
Arslan <Arslan98671@mail.ru>
2015-12-20 17:40:44 +0300

    Update S2.pas

--- S2.pas
+++ S2.pas
@@ -1,65 +1,95 @@
-type
- massive = array of boolean;
+program S2;
+const nmax=1000;
+type index=1..nmax;
+myset=array[index] of boolean;
+var a, b, c: myset; startind, endind: index; i:integer;
 
-procedure mas_max(var arr: massive; l: integer);
-var
- i: integer;
-begin
- setlength(arr, l + 1);
- for i := 0 to l do
- begin
-  arr[i] := false;
- end;
-end;
+function isbelong(ind:index; ms:myset): boolean;
+  begin
+    isbelong:=ms[ind]; 
+  end;
+ 
+function define (startind:index; endind:index; empty: boolean;false:myset);
+  var res:myset; i:integer;
+  begin
+    for i:= 1 to nmax do
+    begin 
+      res[i]:=false;
+    end;
+    for i:= startind to endind do 
+      begin
+        res[i]:= not empty;
+      end;
+      define:=res;
+  end;
 
-procedure mas_add(var arr: massive; a: integer);
-begin
- if a < length(arr) then arr[a] := true;
-end;
+function association ( ms1:myset; ms2 : myset):myset;
+  var res: myset; i:integer;
+  begin
+    for i:=1 to nmax do 
+    begin 
+      res[i]:= (ms1[i]= true) or ( ms2[i]=true);
+    end;
+association:= res;
+  end;
 
-procedure mas_del(var arr: massive; a: integer);
-begin
- if a < length(arr) then arr[a] := false;
-end;
+function intersection (ms1:myset; ms:myset):myset;
+  var res:myset; i:=integer;
+  begin
+    for i:=1 to nmax do 
+    begin
+      res[i] := ( ms1[i]=true) and (ms2[i]=true);
+    end;
+    intersection:=res;
+  end;
+  
+function difference (ms1:myset; ms2: myset):myset;
+ var res:myset; i:integer;
+  begin
+    for i:=1 to nmax do 
+    begin
+      res[i]:= (ms1[i]=true) and (ms2[i]=false);
+    end;
+    difference:=res;
+  end;
 
-procedure mas_show(var arr: massive; a: integer);
-var
- i: integer;
-begin
- for i := 0 to length(arr) - 1 do
- begin
-  if arr[i] then write(i, ' ');
- end;
- writeln;
-end;
+function complement (ms:myset):myset;
+ var res:myset; i: integer;
+  begin
+    for i:=1 to nmax do
+    begin
+      res[i]:= not ms[i];
+    end;
+    complement:= res;
+  end;
 
-procedure read_array(var a: array of integer);
-var
- i, l: integer;
-begin
- write('Length: ');
- readln(l);
- setlength(a, l);
- for i := 0 to l - 1 do
- begin
-  readln(a[i]);
- end;
-end;
+function symmetricdif (ms1:myset; ms2:myset): myset;
+ var res: myset; i:integer;
+  begin
+    for i:=1 to nmax do 
+    begin 
+      res[i]:= not (ms1[i] = true) and (ms2[i] = true);
+    end;
+    symmetricdif :=res;
+  end;
 
-var
- a, i: integer;
- x: array of integer;
- arr: massive;
-
-begin
- write('Enter max of massive: ');
- readln(a);
- mas_max(arr, a);
- writeln('Input array');
- read_array(x);
- for i := 0 to length(x) - 1 do mas_add(arr, x[i]);
- writeln('Delete array');
- read_array(x);
- for i := 0 to length(x) - 1 do mas_del(arr, x[i]);
- mas_show(arr, a);
+function sieveofEra (n:index):myset;
+ var res:myset; i, j:index;
+  begin
+    res:=define (2,n); 
+    i:= 2;
+    while i<=n do
+    begin 
+      j:=2;
+      while i*j <= n do
+      begin
+        res:=difference (res, define (i*j, i*j));
+        inc(j);
+      end;
+      inc(i);
+    end;
+    sieveofEra:=res;
+  end;
+begin  
 end.
+ 
